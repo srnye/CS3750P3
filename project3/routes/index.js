7f8aa2a2e49d3f40a3e5d952b746dbba7dbd1522
srnye <steven.r.nye@gmail.com>
2017-03-19 21:50:03 -0600

    Added routes and views for create and join game

--- project3/routes/index.js
+++ project3/routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Balderdash' });
+});
+
+/* GET game creation page */
+router.get('/newgame', function(req, res){
+  res.render('newgame', {
+    title: 'Create New Game'
+  });
+});
+
+/* GET game joining page */
+router.get('/joingame', function(req, res){
+  res.render('joingame', {
+    title: 'Join Existing Game'
+  });
 });
 
 module.exports = router;
