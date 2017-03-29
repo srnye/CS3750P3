29e18b3d85f5558993aec6ecc62967b1268b319a
Wesley Smith <wesultimatef@gmail.com>
2017-03-29 17:23:09 -0600

    this has the question management functionality

--- project3/app.js
+++ project3/app.js
@@ -13,6 +13,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var game = require('./routes/game');
 var questions = require('./routes/questions');
+var manage = require('./routes/manage');
 
 var app = express();
 
@@ -76,6 +77,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/game', game);
 app.use('/questions', questions);
+app.use('/manage', questions);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
