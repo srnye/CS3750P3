3d0dd36fce9bbb70c46876e2267a61346f5d02dd
Unknown <iansuddreth@gmail.com>
2017-03-29 19:05:17 -0600

    removing unused code

--- project3/public/js/main.js
+++ project3/public/js/main.js
@@ -2,10 +2,6 @@ window.onload = function()
 {
     var messages = [];
     var socket = io.connect('http://localhost:3000');
-    // var field = document.getElementById("chat-text");
-    // var sendButton = document.getElementById("send");
-    // var chat = document.getElementById("chat-messages");
-    // var name = document.getElementById("myName");
     // Passed in variables from game creation
     var gameName = document.getElementById("gameName");
     var numPlayers = document.getElementById("numPlayers");
@@ -14,8 +10,7 @@ window.onload = function()
     var categories = document.getElementById("categories");
     var isNewGame = document.getElementById("isNewGame");
     // Client side variables
-    //field.focus();
-    //var user;
+    
 
     socket.emit('join', 
     { 
