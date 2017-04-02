8b69bab4f1ff1a65407e356150ded1523a622b3d
srnye <steven.r.nye@gmail.com>
2017-04-01 22:24:20 -0600

    More work with Ian

--- project3/public/js/main.js
+++ project3/public/js/main.js
@@ -16,7 +16,27 @@ window.onload = function()
     var waitingDiv = document.getElementById("waitingDiv");
     var waitingTable = document.getElementById("waitingTable");
     var waitingHostLoader = document.getElementById("waitingHostLoader");
+    var waitingAnswerLoader = document.getElementById("waitingAnswerLoader");
+    var waitingGuessesLoader = document.getElementById("waitingGuessesLoader");
     var timer = document.getElementById("timer");
+
+    //HOST DIV
+    var hostDiv = document.getElementById("hostDiv");
+    var catDiv = document.getElementById("catDiv");
+    var hostQDiv = document.getElementById("hostQDiv");
+    var timerHost = document.getElementById("timerHost");
+
+    //WRITE ANSWER DIV
+    var writeAnswerDiv = document.getElementById("writeAnswerDiv");
+    var questionHeader = document.getElementById("questionHeader");
+    var warningMsg = document.getElementById("warningMsg");
+    var answerTxt = document.getElementById("answerTxt");
+    var answerSubmitBtn = document.getElementById("answerSubmitBtn");
+
+    //GUESS DIV
+    var guessAnswerDiv = document.getElementById("guessAnswerDiv");
+    var answerGuessHeader = document.getElementById("answerGuessHeader");
+    var guessDiv = document.getElementById("guessDiv");
 
     socket.emit('join', 
     { 
@@ -46,9 +66,24 @@ window.onload = function()
         waitingHostLoader.style.display = 'block';
     });
     
-    socket.on('hostScreen', function()
+    socket.on('hostScreen', function(data)
     {
-        
+        //hide loader
+        waitingHostLoader.style.display = 'none';
+        //show host div
+        hostDiv.style.display = 'block';
+
+        for (var c in data.categories)
+        {
+            var button = document.createElement("button");
+            button.className = "btn btn-secondary";
+            button.innerHTML = data.categories[c];
+            button.onclick = function()
+            {
+                socket.emit('categoryChanged', {catName: this.innerHTML, gameName: gameName.value});
+            };
+            catDiv.appendChild(button);
+        }
     });
 
     //timer
@@ -57,12 +92,106 @@ window.onload = function()
         timerStart(data.countdown);
     });
 
+    socket.on('showHostQuestions', function(data)
+    {
+        //clear previous questions
+        hostQDiv.innerHTML = '';
+        //add new questions
+        for (var q in data.questions)
+        {
+            var button = document.createElement("button");
+            button.className = "btn btn-primary";
+            button.innerHTML = data.questions[q].question;
+            button.onclick = function()
+            {
+                socket.emit('questionChosen', {question: this.innerHTML, gameName: gameName.value});
+            };
+            hostQDiv.appendChild(button);
+        }
+    });
+
+    socket.on('writeAnswer', function(data)
+    {
+        //hide animation
+        waitingHostLoader.style.display = 'none';
+        //hide host screen
+        hostDiv.style.display = 'none';
+        //show write answer div
+        writeAnswerDiv.style.display = 'block';
+        //update divs
+        questionHeader.innerHTML = data.question;
+
+        answerSubmitBtn.onclick = function()
+        {
+            //check if text is null or empty
+            if (answerTxt.value == "" || answerTxt.value == null)
+            {
+                warningMsg.style.display = 'block';
+                warningMsg.innerHTML = "Please enter an answer";
+                return;
+            }
+            socket.emit('answerSubmitted',
+            {
+                playerName: playerName.value,               
+                gameName: gameName.value,
+                answer: answerTxt.value
+            });
+        }
+    });
+
+    socket.on('waitForAnswers', function()
+    {
+        //hide answser div
+        writeAnswerDiv.style.display = 'none';
+        //show loader
+        waitingAnswerLoader.style.display = 'block';
+
+    });
+
+    socket.on('guessAnswer', function(data)
+    {
+        //hide answer div
+        writeAnswerDiv.style.display = 'none';
+        //hide loader
+        waitingAnswerLoader.style.display = 'none';
+
+        guessAnswerDiv.style.display = 'block';
+        guessDiv.innerHTML = '';
+        answerGuessHeader.innerHTML = data.question.question;
+
+        for (var a in data.answers)
+        {
+            var button = document.createElement("button");
+            button.className = "btn btn-primary";
+            button.innerHTML = data.answers[a].answer;
+            button.onclick = function()
+            {
+                alert("Answer Originator: " + data.answers[a].player + " Answer :" + data.answers[a].answer);
+                //socket.emit('answerChosen', {answer: data.answers[a], gameName: gameName.value, player: playerName.value});
+            };
+
+            if (data.answers[a].player != playerName.value)
+            {
+                guessDiv.appendChild(button);
+            }
+        }
+    });
+
+    socket.on('waitForGuesses', function()
+    {
+        //hide quess answer div
+        guessAnswerDiv.style.display = 'none';
+        //show loader
+        waitingGuessesLoader.style.display = 'block';
+    });
+
     
 function timerStart(countdown) {
     //----------------- TIMER ---------------------
         setInterval(function() {  
         countdown--;
         timer.innerHTML = countdown;
+        timerHost.innerHTML = countdown;
         }, 1000);
 }
 
--- project3/routes/server.js
+++ project3/routes/server.js
@@ -29,11 +29,20 @@ io.sockets.on('connection', function(socket)
                 categories: obj.categories,
                 players: [],
                 currentHost: {},
+                currentQuestion: {},
                 roundsPlayed: 0,
-                questions: obj.questions
+                questions: obj.questions,
+                gameInterval: 0,
+                answers: [],
+                playersReady: []
             };
 
-            var player = {name: obj.playerName, score: 0, socketID: socket.id};
+            var player = 
+            {
+                name: obj.playerName, 
+                score: 0, 
+                socketID: socket.id
+            };
             game.players.push(player);
             games[obj.gameName] = game;
         }
@@ -62,15 +71,6 @@ io.sockets.on('connection', function(socket)
             
             //start round
             beginRound(obj.gameName);
-
-
-            //----------------- TIMER ---------------------
-
-            var countdown = 60;
-            io.in(obj.gameName).emit('timer', { countdown: countdown });
-            setInterval(function() {  
-            countdown--;
-            }, 1000);
         }
         //if not, update waiting lobby
         else
@@ -95,82 +95,227 @@ io.sockets.on('connection', function(socket)
 
     socket.on('categoryChanged', function(obj)
     {
-
-    });
-});
-
-//----------------- FUNCTIONS -----------------
-
-function searchPlayer(name) 
-{
-    for(var i = 0; i < players.length; i++) {
-        if(players[i].name == name) {
-            return i
-        }
-    }
-    return -1
-}
-
-function randomInt (low, high) 
-{
-    return Math.floor(Math.random() * (high - low) + low);
-}
-
-function createQuestions(cats, qs)
-{
-    var questions = [];
-
-    cats = JSON.parse(cats);
-    qs = JSON.parse(qs);
-
-    for (var c in cats)
-    {
-        for(var q in qs)
+        var qs = [];
+        var i = 0;
+        for (var q in games[obj.gameName].questions)
         {
-            if (qs[q].category == cats[c])
+            if (i == 3)
             {
-                var quest = {question: qs[q].question, answer: qs[q].answer};
-                questions.push(quest);
+                break;
+            }
+            if (games[obj.gameName].questions[q].category == obj.catName)
+            {
+                qs.push(games[obj.gameName].questions[q]);
+                i++;
             }
         }
+        io.to(socket.id).emit('showHostQuestions', {questions: qs}); 
+    });
+
+    socket.on('questionChosen', function(obj)
+    {
+        stopTimer(obj.gameName);
+        //find index of question to remove
+        var index = 0;
+        for (var q in games[obj.gameName].questions)
+        {
+            if (obj.question == games[obj.gameName].questions[q].question)
+            {
+                break;
+            }
+            index++;
+        }
+        //set current question
+        games[obj.gameName].currentQuestion = games[obj.gameName].questions[index];
+        //remove question from questions array
+        games[obj.gameName].questions.splice(index,1);
+        //emit question to all players (including host) in answer div
+        io.in(obj.gameName).emit('writeAnswer', {question: obj.question});
+    });
+
+    socket.on('answerSubmitted', function(obj)
+    {
+        //update game information
+        var answer = {player: obj.playerName, answer: obj.answer};
+        games[obj.gameName].answers.push(answer);
+        //check to see if all players submitted answers
+        //if yes, send to guessing div
+        if (games[obj.gameName].answers.length == games[obj.gameName].numPlayers)
+        {
+            var answer = {player: "", answer: games[obj.gameName].currentQuestion.answer};
+            games[obj.gameName].answers.push(answer);
+            io.in(obj.gameName).emit('guessAnswer', 
+            {
+                question: games[obj.gameName].currentQuestion,
+                answers: shuffle(games[obj.gameName].answers),
+                gameName: obj.gameName
+            });
+        }
+        //if not, emit waiting div
+        else
+        {
+            socket.emit('waitForAnswers');
+        }
+    });
+
+    socket.on('answerChosen', function(obj)
+    {
+        //1 point if another user quesses your answer
+        //2 points if you guess the correct answer
+        
+        //check if another player created answer
+        var index = 0;
+        for (var p in games[obj.gameName].players)
+        {
+            if (games[obj.gameName].players[p].name == obj.player)
+            {
+                index = p;
+                break;
+            }
+        }
+        if (obj.answer.player == "")
+        {
+            games[obj.gameName].players[index].score += 2;
+        }
+        else
+        {
+            index = 0;
+            for (var p in games[obj.gameName].players)
+            {
+                if (games[obj.gameName].players[p].name == obj.answer.player)
+                {
+                    break;
+                }
+                index++;
+            }
+             games[obj.gameName].players[index].score += 1;
+        }
+
+        games[obj.gameName].playersReady.push(obj.player);
+
+        if (games[obj.gameName].playersReady.length == games[obj.gameName].numPlayers)
+        {
+            //emit question results screen
+            io.in(obj.gameName).emit('questionResults', {players: games[obj.gameName].players});
+        }
+        else
+        {
+            //emit waiting screen
+            socket.emit('waitForGuesses');
+        }
+    });
+
+    //----------------- FUNCTIONS -----------------
+
+    function searchPlayer(name) 
+    {
+        for(var i = 0; i < players.length; i++) {
+            if(players[i].name == name) {
+                return i
+            }
+        }
+        return -1
     }
 
-    questions = shuffle(questions);
-    return questions;
-}
+    function randomInt (low, high) 
+    {
+        return Math.floor(Math.random() * (high - low) + low);
+    }
 
-function shuffle(array) 
-{
-  var currentIndex = array.length, temporaryValue, randomIndex;
+    function createQuestions(cats, qs)
+    {
+        var questions = [];
 
-  // While there remain elements to shuffle...
-  while (0 !== currentIndex) 
-  {
+        cats = JSON.parse(cats);
+        qs = JSON.parse(qs);
 
-    // Pick a remaining element...
-    randomIndex = Math.floor(Math.random() * currentIndex);
-    currentIndex -= 1;
+        for (var c in cats)
+        {
+            for(var q in qs)
+            {
+                if (qs[q].category == cats[c])
+                {
+                    questions.push(qs[q]);
+                }
+            }
+        }
 
-    // And swap it with the current element.
-    temporaryValue = array[currentIndex];
-    array[currentIndex] = array[randomIndex];
-    array[randomIndex] = temporaryValue;
-  }
+        questions = shuffle(questions);
+        return questions;
+    }
 
-  return array;
-}
+    function shuffle(array) 
+    {
+        var currentIndex = array.length, temporaryValue, randomIndex;
 
-function beginRound(gameName)
-{
-    //set currentHost
-    var host = {name: games[gameName].players[0].name, socketID: games[gameName].players[0].socketID};
-    games[gameName].currentHost = host; 
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) 
+        {
 
-    //have other players wait - TODO: show timer
-    io.in(gameName).emit('waitForHost');
-    
-    //TODO: have host pick question (timed)
-    socket.to(games[gameName].currentHost.socketID).emit('hostScreen');
-}
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+
+        return array;
+    }
+
+    function startHostTimer(seconds, gameName)
+    {
+        var countdown = seconds;
+        //emit client side countdown
+        io.in(gameName).emit('timer', { countdown: seconds });
+        //countdown server side
+        games[gameName].gameInterval = setInterval(function() 
+        {  
+            countdown--;
+            if (countdown == 0)
+            {
+                stopTimer(gameName);
+                //choose random question
+                games[gameName].currentQuestion = games[gameName].questions[0];
+                //remove question from array
+                games[gameName].questions.splice(0,1);
+                //emit question to all players (including host) in answer div
+                io.in(gameName).emit('writeAnswer', {question: games[gameName].currentQuestion});
+            }
+        }, 1000);
+    }
+
+    function stopTimer(gameName)
+    {
+        clearInterval(games[gameName].gameInterval);
+    }
+
+    function beginRound(gameName)
+    {
+        //set currentHost
+        var host = {name: games[gameName].players[0].name, socketID: games[gameName].players[0].socketID};
+        games[gameName].currentHost = host; 
+
+        //clear questions array
+        games[gameName].answers = [];
+
+        //clear ready array
+        games[gameName].playersReady = [];
+
+        //have other players wait - TODO: show timer
+        io.in(gameName).emit('waitForHost');
+        
+        //TODO: have host pick question (timed)
+        socket.to(games[gameName].currentHost.socketID).emit('hostScreen', 
+        {
+            categories: JSON.parse(games[gameName].categories),
+            questions: games[gameName].questions
+        });
+
+        startHostTimer(60, gameName);
+    }
+});
 
 } //end exports
